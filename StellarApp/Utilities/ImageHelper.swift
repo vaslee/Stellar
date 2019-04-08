35ff01800c2ba0c57cf09ed93c80e3695e09a5e9
vaslee <ltx0503@gmail.com>
2019-04-08 16:48:49 -0400

    Setting up all the files and textures

---
+++ StellarApp/Utilities/ImageHelper.swift
@@ -0,0 +1,9 @@
+//
+//  ImageHelper.swift
+//  StellarApp
+//
+//  Created by TingxinLi on 4/8/19.
+//  Copyright © 2019 TingxinLi. All rights reserved.
+//
+
+import Foundation
---
+++ StellarApp/Utilities/NewsAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  NewsAPIClient.swift
+//  StellarApp
+//
+//  Created by TingxinLi on 4/8/19.
+//  Copyright © 2019 TingxinLi. All rights reserved.
+//
+
+import Foundation
