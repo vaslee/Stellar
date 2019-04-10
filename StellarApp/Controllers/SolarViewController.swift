c86b3734ff20c644cf6279c29a4b78896a59044c
vaslee <ltx0503@gmail.com>
2019-04-10 16:24:13 -0400

    update the solarVC, having all the planets and orbit setting up in ARKit

--- StellarApp/Controllers/SolarViewController.swift
+++ StellarApp/Controllers/SolarViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 import SceneKit
 import ARKit
 
-class SolarViewController: UIViewController, ARSCNViewDelegate {
-
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    let sun = SCNSphere(radius: 0.5)
+    let meterial = SCNMaterial()
+    let sunNode = SCNNode()
+    
+    private var planetObjects = [Planet]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +29,32 @@ class SolarViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        setUpSun()
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        planetObjects = Planet.loadPlanets
+        
+        // TESTING CODE:
+        //let node = Planet.getNode(image: UIImage(named: "art.scnassets/mercury.jpg")!, radius: 0.2, vector: (x: 1.3, y: -1, z: 0))
+        
+        
+        
+        planetObjects.forEach { sceneView.scene.rootNode.addChildNode($0.planetNode) }
+        
+        // TESTING CODE:
+        // let ring = Planet.getRing(radius: 2, vector: (x: 0, y: -1, z: 0), color: .yellow)
+        // planetObjects.forEach { sceneView.scene.rootNode.addChildNode($0.ringNode) }
+        //sceneView.scene.rootNode.addChildNode(node)
+        
+    }
+    
+    func setUpSun() {
+        
+        meterial.diffuse.contents = UIImage(named: "art.scnassets/sun.jpg")
+        sun.materials = [meterial]
+        sunNode.position = SCNVector3(x: 0, y: -1, z: 0)
+        sunNode.geometry = sun
+        sceneView.scene.rootNode.addChildNode(sunNode)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +62,7 @@ class SolarViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,30 +73,5 @@ class SolarViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
