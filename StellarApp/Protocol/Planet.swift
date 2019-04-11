25a568123a6327f7e8140cfc4b6a1403552bc3e5
vaslee <ltx0503@gmail.com>
2019-04-11 17:18:01 -0400

    refactor entire code

--- StellarApp/Protocol/Planet.swift
+++ StellarApp/Protocol/Planet.swift
@@ -8,58 +8,110 @@
 
 import UIKit
 import ARKit
+enum PlanetType: CaseIterable{
+    case mercury
+    case venus
+    case earth
+    case mars
+    case jupiter
+    case saturn
+    case uranus
+    case neptune
+}
 
-struct Planet: PlanetaryBody {
+
+struct Planet {
     
-    var name: String
-    var planetNode: SCNNode
+    static var planetNodes = [SCNNode]()
     
-     /*deleting later*/// "mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"
-    static let loadPlanets: [Planet] = [
-        
-        Planet(name: "mercury", planetNode: getNode(image: UIImage(named: "art.scnassets/mercury.jpg")!, radius: 0.2, vector: (x: 1.3, y: 0, z: 0), orbitRadius: 1.3, moveSpeed: 1.2, rotation: 1.6)),
-        Planet(name: "venus", planetNode: getNode(image: UIImage(named: "art.scnassets/venus.jpg")!, radius: 0.4, vector: (x: 2, y: 0, z: 0), orbitRadius: 2, moveSpeed: 0.8, rotation: 0.9)),
-        Planet(name: "earth", planetNode: getNode(image: UIImage(named: "art.scnassets/earth.jpg")!, radius: 0.5, vector: (x: 3, y: 0, z: 0), orbitRadius: 3, moveSpeed: 0.5, rotation: 1.2)),
-        
-        Planet(name: "mars", planetNode: getNode(image: UIImage(named: "art.scnassets/mars.jpg")!, radius: 0.4, vector: (x: 4.2, y: 0, z: 0), orbitRadius: 4.2, moveSpeed: 0.4, rotation: 0.8)),
-        
-        Planet(name: "jupiter", planetNode: getNode(image: UIImage(named: "art.scnassets/jupiter.jpg")!, radius: 0.9, vector: (x: 6, y: 0, z: 0), orbitRadius: 6, moveSpeed: 0.9, rotation: 1.3)),
-        
-        Planet(name: "saturn", planetNode: getNode(image: UIImage(named: "art.scnassets/saturn.jpg")!, radius: 0.7, vector: (x: 8, y: 0, z: 0), orbitRadius: 8, moveSpeed: 0.7, rotation: 0.2)),
-        
-        Planet(name: "uranus", planetNode: getNode(image: UIImage(named: "art.scnassets/uranus.jpg")!, radius: 0.8, vector: (x: 10, y: 0, z: 0), orbitRadius: 10, moveSpeed: 0.5, rotation: 0.4)),
-        
-        Planet(name: "neptune", planetNode: getNode(image: UIImage(named: "art.scnassets/neptune.jpg")!, radius: 0.6, vector: (x: 12, y: 0, z: 0), orbitRadius: 12, moveSpeed: 0.4, rotation: 0.8))
-        
-        
-        //TESTING CODE
-        //                Planet(name: "mercury", planetNode: getNode(image: UIImage(named: "art.scnassets/mercury.jpg")!, radius: 0.2, vector: (x: 1.3, y: 0, z: 0), orbitRadius: 1.3, moveSpeed: 1.2, rotation: 1.6))
-        
-        
-    ]
+    static func getPlanets() -> [SCNNode] {
+        for planetType in PlanetType.allCases {
+            switch planetType {
+            case .mercury:
+                let orbitNode = getRing(radius: 1, vector: (x: 0, y: -1, z: 0), color: .yellow)
+                let planetNode = getNode(name: "mercury", image: UIImage(named: "art.scnassets/mercury.jpg")!, radius: 0.2, vector: (x: 1, y: 0, z: 0), moveSpeed: 1.2, rotation: 1.6)
+                orbitNode.addChildNode(planetNode)
+                orbitNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 1.2, z: 0, duration: 1)))
+                planetNodes.append(orbitNode)
+                
+            case .venus:
+                let orbitNode = getRing(radius: 1.8, vector: (x: 0, y: -1, z: 0), color: .yellow)
+                let planetNode = getNode(name: "venus", image: UIImage(named: "art.scnassets/venus.jpg")!, radius: 0.4, vector: (x: 1.8, y: 0, z: 0), moveSpeed: 0.8, rotation: 0.9)
+                orbitNode.addChildNode(planetNode)
+                orbitNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.8, z: 0, duration: 1)))
+                planetNodes.append(orbitNode)
+                
+            case .earth:
+                let orbitNode = getRing(radius: 3, vector: (x: 0, y: -1, z: 0), color: .yellow)
+                let planetNode = getNode(name: "earth", image: UIImage(named: "art.scnassets/earth.jpg")!, radius: 0.5, vector: (x: 3, y: 0, z: 0), moveSpeed: 0.5, rotation: 1.2)
+                let moonOrbitNode = getRing(radius: 0.8, vector: (x: 0, y: 0, z: 0), color: .blue)
+                let moonNode = getNode(name: "moon", image: UIImage(named:"art.scnassets/moon.jpg")!, radius: 0.2, vector: (x: -0.8, y: 0, z: 0), moveSpeed: 0.2, rotation: 1.6)
+                
+                orbitNode.addChildNode(planetNode)
+                planetNode.addChildNode(moonOrbitNode)
+                moonOrbitNode.addChildNode(moonNode)
+                orbitNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.5, z: 0, duration: 1)))
+                planetNodes.append(orbitNode)
+                
+            case .mars:
+                let orbitNode = getRing(radius: 4.2, vector: (x: 0, y: -1, z: 0), color: .yellow)
+                let planetNode = getNode(name: "mars", image: UIImage(named: "art.scnassets/mars.jpg")!, radius: 0.4, vector: (x: 4.2, y: 0, z: 0), moveSpeed: 0.4, rotation: 0.8)
+                orbitNode.addChildNode(planetNode)
+                orbitNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.4, z: 0, duration: 1)))
+                planetNodes.append(orbitNode)
+                
+            case .jupiter:
+                let orbitNode = getRing(radius: 6, vector: (x: 0, y: -1, z: 0), color: .yellow)
+                let planetNode = getNode(name: "jupiter", image: UIImage(named: "art.scnassets/jupiter.jpg")!, radius: 0.9, vector: (x: 6, y: 0, z: 0), moveSpeed: 0.9, rotation: 1.3)
+                orbitNode.addChildNode(planetNode)
+                orbitNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.9, z: 0, duration: 1)))
+                planetNodes.append(orbitNode)
+                
+            case .saturn:
+                let orbitNode = getRing(radius: 8, vector: (x: 0, y: -1, z: 0), color: .yellow)
+                let planetNode = getNode(name: "saturn", image: UIImage(named: "art.scnassets/saturn.jpg")!, radius: 0.7, vector: (x: 8, y: 0, z: 0), moveSpeed: 0.7, rotation: 0.2)
+                let saturnOrbitNode = getRing(radius: 0.9, vector: (x: 0, y: 0, z: 0), color: .red)
+                orbitNode.addChildNode(planetNode)
+                planetNode.addChildNode(saturnOrbitNode)
+                orbitNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.7, z: 0, duration: 1)))
+                planetNodes.append(orbitNode)
+                
+            case .uranus:
+                let orbitNode = getRing(radius: 10, vector: (x: 0, y: -1, z: 0), color: .yellow)
+                let planetNode = getNode(name: "uranus", image: UIImage(named: "art.scnassets/uranus.jpg")!, radius: 0.8, vector: (x: 10, y: 0, z: 0), moveSpeed: 0.5, rotation: 0.4)
+                orbitNode.addChildNode(planetNode)
+                orbitNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.5, z: 0, duration: 1)))
+                planetNodes.append(orbitNode)
+                
+            case .neptune:
+                let orbitNode = getRing(radius: 12, vector: (x: 0, y: -1, z: 0), color: .yellow)
+                let planetNode = getNode(name: "neptune", image: UIImage(named: "art.scnassets/neptune.jpg")!, radius: 0.6, vector: (x: 12, y: 0, z: 0), moveSpeed: 0.4, rotation: 0.8)
+                orbitNode.addChildNode(planetNode)
+                orbitNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.4, z: 0, duration: 1)))
+                planetNodes.append(orbitNode)
+                
+            default:
+                break
+            }
+        }
+        return planetNodes
+    }
     
-    
-    static func getNode(image: UIImage,
+    static func getNode(name: String, image: UIImage,
                         radius: CGFloat,
-                        vector: (x: Float, y: Float, z: Float), orbitRadius: CGFloat, moveSpeed: CGFloat, rotation: CGFloat) -> SCNNode{
-        // TODO: if ring node first
-        let orbitNode = getRing(radius: orbitRadius, vector: (x: 0, y: -1, z: 0), color: .yellow)
+                        vector: (x: Float, y: Float, z: Float), /*orbitRadius: CGFloat,*/ moveSpeed: CGFloat, rotation: CGFloat/*, moonOrbitRadius: CGFloat*/) -> SCNNode{
+        
         let node = SCNNode()
         let nodeSize = SCNSphere(radius: radius)
         let material = SCNMaterial()
-        
         material.diffuse.contents = image
+        node.name = name
         nodeSize.materials = [material]
         node.position = SCNVector3(x: vector.x, y: vector.y, z: vector.z)
         node.geometry = nodeSize
-        
-        orbitNode.addChildNode(node)
-        
-        orbitNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: moveSpeed, z: 0, duration: 1)))
         node.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: rotation, z: 0, duration: 1)))
         
-        return orbitNode
-        
+        return node
     }
     
     static func getRing(radius: CGFloat,
@@ -78,17 +130,4 @@ struct Planet: PlanetaryBody {
         return ringNode
     }
     
-    static private func generateTexture(image: UIImage) -> SCNMaterial {
-        let material = SCNMaterial()
-        material.diffuse.contents = image
-        return material
-    }
-    
-    //    func getPlanet(_ name: String) -> Planet? {
-    //        let results = Planet.loadPlanets.filter { $0.name == name }
-    //        guard let planet = results.first else {
-    //            return nil
-    //        }
-    //        return planet
-    //    }
 }
